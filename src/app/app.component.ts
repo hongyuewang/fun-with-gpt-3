15a1e28ab206c878747519c879006bc5aa75b049
Hong Yue Wang <hongyuew26@gmail.com>
2022-05-11 18:45:05 -0400

    Add Angular files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fun-with-gpt3';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClickSubmit(formData: { prompt: string; }) {
+    const data = {
+      prompt: formData.prompt,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+  };
+
+  
+ const res = fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+  mode: 'cors',
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json",
+ Authorization: `Bearer ${environment.apiKey}`
+  },
+  body: JSON.stringify(data)
+});
+  
+ }
+
 }
