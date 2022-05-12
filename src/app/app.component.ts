93b3fd007cae4d9102b021676efb69b24e02c3a5
Hong Yue Wang <hongyuew26@gmail.com>
2022-05-11 21:38:27 -0400

    Add responses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,18 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fun-with-gpt3';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onClickSubmit(formData: { prompt: string; }) {
+  htmlToAdd = "";
+
+  async onClickSubmit(formData: { prompt: string }) {
     const data = {
       prompt: formData.prompt,
       temperature: 0.5,
@@ -23,19 +24,34 @@ export class AppComponent {
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
-  };
+    };
 
-  
- const res = fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-  mode: 'cors',
-  method: "POST",
-  headers: {
-      "Content-Type": "application/json",
- Authorization: `Bearer ${environment.apiKey}`
-  },
-  body: JSON.stringify(data)
-});
-  
- }
+    let responseText = "";
 
+    const res = fetch(
+      'https://api.openai.com/v1/engines/text-curie-001/completions',
+      {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${environment.apiKey}`,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    responseText = await res.then(value => value.json()).then(function(value) {
+      return value.choices[0].text;
+    });
+
+    this.htmlToAdd += `<table class="table">
+    <thead></thead>
+    <tbody>
+    <tr><th scope="row">Prompt</th><td>${formData.prompt}</td></tr>
+    <tr><th scope="row">Response</th><td>${responseText}</td></tr>
+    </tbody>
+    </table>
+    <br>`
+  }
 }
